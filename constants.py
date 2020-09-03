b6a6f120bd4cf1a0b7b07d6ab54566d1d3bc9ea0
SebastJava <sebastjava@hotmail.ca>
2020-09-03 13:16:43 -0400

    Colors update
    	modified:   Mint-Y-Colors/Mint-Y-Variations-NEW.png
    	modified:   Mint-Y-Colors/Mint-Y-Variations-NEW.svg
    	modified:   Mint-Y-Colors/compare/Mint-Y-Variations-compare-100.svg
    	modified:   Mint-Y-Colors/compare/Mint-Y-Variations-compare-old.png
    	modified:   Mint-Y-Colors/compare/Mint-Y-Variations-compare-old.svg
    	modified:   constants.py

--- constants.py
+++ constants.py
@@ -1,36 +1,5 @@
 #!/usr/bin/python3
 
-# MINT-X
-
-X_HEX_ACCENTS = ["#9ab87c", "#accd8a"]
-
-x_hex_colors = {}
-x_hex_colors["Aqua"] = "#6cabcd"
-x_hex_colors["Blue"] = "#5b73c4"
-x_hex_colors["Brown"] = "#aa876a"
-x_hex_colors["Grey"] = "#9d9d9d"
-x_hex_colors["Orange"] = "#db9d61"
-x_hex_colors["Pink"] = "#c76199"
-x_hex_colors["Purple"] = "#8c6ec9"
-x_hex_colors["Red"] = "#c15b58"
-x_hex_colors["Sand"] = "#c8ac69"
-x_hex_colors["Teal"] = "#5aaa9a"
-
-X_RGB_ACCENTS = ["172, 205, 138", "#accd8a"]
-
-x_rgb_colors = {}
-x_rgb_colors["Aqua"] = "108, 171, 205"
-x_rgb_colors["Blue"] = "91, 115, 196"
-x_rgb_colors["Brown"] = "170, 135, 106"
-x_rgb_colors["Grey"] = "157,157,157"
-x_rgb_colors["Orange"] = "219, 157, 97"
-x_rgb_colors["Pink"] = "199, 97, 153"
-x_rgb_colors["Purple"] = "140, 110, 201"
-x_rgb_colors["Red"] = "193, 91, 88"
-x_rgb_colors["Sand"] = "200, 172, 105"
-x_rgb_colors["Teal"] = "90, 170, 154"
-
-
 # MINT-Yz
 
 # colors1 IS THE MAIN ACCENT COLOR for the light themes.
@@ -49,6 +18,26 @@ y_hex_colors2 = {}
 y_hex_colors3 = {}
 y_hex_colors4 = {}
 
+y_hex_colors1["MintFresh"] = "#22AA4F"
+y_hex_colors2["MintFresh"] = "#1D9143"
+y_hex_colors3["MintFresh"] = "#4EBB72"
+y_hex_colors4["MintFresh"] = "#187737"
+
+y_hex_colors1["Mint"] = "#18AA60"
+y_hex_colors2["Mint"] = "#149152"
+y_hex_colors3["Mint"] = "#46BB80"
+y_hex_colors4["Mint"] = "#117743"
+
+y_hex_colors1["MintDrink"] = "#00A870"
+y_hex_colors2["MintDrink"] = "#008F5F"
+y_hex_colors3["MintDrink"] = "#33B98D"
+y_hex_colors4["MintDrink"] = "#00764F"
+
+y_hex_colors1["Grey"] = "#767676"
+y_hex_colors2["Grey"] = "#767676"
+y_hex_colors3["Grey"] = "#919191"
+y_hex_colors4["Grey"] = "#535353"
+
 y_hex_colors1["Red"] = "#DF2131"
 y_hex_colors2["Red"] = "#BE1C2A"
 y_hex_colors3["Red"] = "#E54D5A"
@@ -58,16 +47,6 @@ y_hex_colors1["Orange"] = "#F46A25"
 y_hex_colors2["Orange"] = "#D05A1F"
 y_hex_colors3["Orange"] = "#F68851"
 y_hex_colors4["Orange"] = "#AB4A1A"
-
-y_hex_colors1["MintFresh"] = "#22AA4F"
-y_hex_colors2["MintFresh"] = "#1D9143"
-y_hex_colors3["MintFresh"] = "#4EBB72"
-y_hex_colors4["MintFresh"] = "#187737"
-
-y_hex_colors1["Mint"] = "#00A870"
-y_hex_colors2["Mint"] = "#008F5F"
-y_hex_colors3["Mint"] = "#33B98D"
-y_hex_colors4["Mint"] = "#00764F"
 
 y_hex_colors1["Aqua"] = "#00A2C2"
 y_hex_colors2["Aqua"] = "#008AA5"
@@ -79,10 +58,10 @@ y_hex_colors2["Blue"] = "#1A65B3"
 y_hex_colors3["Blue"] = "#4B92DB"
 y_hex_colors4["Blue"] = "#155493"
 
-y_hex_colors1["BlueDeep"] = "#3F59D9"
-y_hex_colors2["BlueDeep"] = "#364CB9"
-y_hex_colors3["BlueDeep"] = "#657AE1"
-y_hex_colors4["BlueDeep"] = "#2C3E98"
+y_hex_colors1["BlueDeep"] = "#2A47D5"
+y_hex_colors2["BlueDeep"] = "#2A47D5"
+y_hex_colors3["BlueDeep"] = "#556CDD"
+y_hex_colors4["BlueDeep"] = "#1D3296"
 
 y_hex_colors1["PurpleDeep"] = "#731DC8"
 y_hex_colors2["PurpleDeep"] = "#731DC8"
@@ -99,7 +78,3 @@ y_hex_colors2["Pink"] = "#C02F5F"
 y_hex_colors3["Pink"] = "#E85F8D"
 y_hex_colors4["Pink"] = "#9F274F"
 
-y_hex_colors1["Grey"] = "#767676"
-y_hex_colors2["Grey"] = "#767676"
-y_hex_colors3["Grey"] = "#919191"
-y_hex_colors4["Grey"] = "#535353"
