ddb6059e0273704b5128966ed36048d90878f705
SebastJava <sebastjava@hotmail.ca>
2020-09-05 16:52:19 -0400

    Version 2.0: new color, changed colors, rounded corners, colored focus, etc.
    	modified:   0-colors-branch-icon.png
    	modified:   0-colors-branch-icon.svg
    	modified:   Mint-Y-Colors/0-colors-branch-icon-sqr.png
    	modified:   Mint-Y-Colors/0-colors-branch-icon-sqr.svg
    	modified:   Mint-Y-Colors/Mint-Y-Variations-NEW.png
    	modified:   Mint-Y-Colors/Mint-Y-Variations-NEW.svg
    	modified:   Mint-Y-Colors/compare/Mint-Y-Variations-compare-100.svg
    	modified:   Mint-Y-Colors/compare/Mint-Y-Variations-compare-old.png
    	modified:   Mint-Y-Colors/compare/Mint-Y-Variations-compare-old.svg
    	modified:   Mint-Y-Colors/messages-colors.svg
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Blue-d.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Blue-l.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-BlueDeep-d.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-BlueDeep-l.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Mint-d.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Mint-l.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-MintSparkling-d.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-MintSparkling-l.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Orange-d.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Orange-l.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Red-d.png
    	new file:   Mint-Y-Colors/png-src/preview-nemo-Red-l.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-blue-d.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-blue-l.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-mint-d.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-mint-l.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-mintFresh-d.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-mintFresh-l.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-orange-d.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-orange-l.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-red-d.png
    	deleted:    Mint-Y-Colors/png-src/preview-nemo-red-l.png
    	new file:   Mint-Y-Colors/preview-nemo-Mint.png
    	new file:   Mint-Y-Colors/preview-nemo-MintSparkling.png
    	deleted:    Mint-Y-Colors/preview-nemo-mint.png
    	new file:   Mint-Y-Colors/preview-nemo-plg-Blue.png
    	new file:   Mint-Y-Colors/preview-nemo-plg-Mint.png
    	new file:   Mint-Y-Colors/preview-nemo-plg-MintSparkling.png
    	new file:   Mint-Y-Colors/preview-nemo-plg-Orange.png
    	new file:   Mint-Y-Colors/preview-nemo-plg-Red.png
    	deleted:    Mint-Y-Colors/preview-nemo-plg-blue.png
    	deleted:    Mint-Y-Colors/preview-nemo-plg-mint.png
    	deleted:    Mint-Y-Colors/preview-nemo-plg-mintFresh.png
    	deleted:    Mint-Y-Colors/preview-nemo-plg-orange.png
    	deleted:    Mint-Y-Colors/preview-nemo-plg-red.png
    	modified:   Mint-Y-Colors/preview-nemo-plg.svg
    	deleted:    Mint-Y-Colors/preview-nemo.svg
    	modified:   constants.py
    	modified:   debian/changelog
    	modified:   src/Mint-Y/gtk-3.0/gtk-dark.css
    	modified:   src/Mint-Y/gtk-3.0/gtk-darker.css
    	modified:   src/Mint-Y/gtk-3.0/gtk.css
    	modified:   src/Mint-Y/gtk-3.0/sass/_applications.scss
    	modified:   src/Mint-Y/gtk-3.0/sass/_colors.scss
    	modified:   src/Mint-Y/gtk-3.0/sass/_common.scss

--- constants.py
+++ constants.py
@@ -18,20 +18,15 @@ y_hex_colors2 = {}
 y_hex_colors3 = {}
 y_hex_colors4 = {}
 
-y_hex_colors1["MintFresh"] = "#22AA4F"
-y_hex_colors2["MintFresh"] = "#1D9143"
-y_hex_colors3["MintFresh"] = "#4EBB72"
-y_hex_colors4["MintFresh"] = "#187737"
-
 y_hex_colors1["Mint"] = "#18AA60"
-y_hex_colors2["Mint"] = "#149152"
+y_hex_colors2["Mint"] = "#169957"
 y_hex_colors3["Mint"] = "#46BB80"
 y_hex_colors4["Mint"] = "#117743"
 
-y_hex_colors1["MintDrink"] = "#00A870"
-y_hex_colors2["MintDrink"] = "#008F5F"
-y_hex_colors3["MintDrink"] = "#33B98D"
-y_hex_colors4["MintDrink"] = "#00764F"
+y_hex_colors1["MintSparkling"] = "#0AC266"
+y_hex_colors2["MintSparkling"] = "#09AF5C"
+y_hex_colors3["MintSparkling"] = "#3BCE85"
+y_hex_colors4["MintSparkling"] = "#078848"
 
 y_hex_colors1["Grey"] = "#767676"
 y_hex_colors2["Grey"] = "#767676"
@@ -39,22 +34,22 @@ y_hex_colors3["Grey"] = "#919191"
 y_hex_colors4["Grey"] = "#535353"
 
 y_hex_colors1["Red"] = "#DF2131"
-y_hex_colors2["Red"] = "#BE1C2A"
+y_hex_colors2["Red"] = "#DF2131"
 y_hex_colors3["Red"] = "#E54D5A"
 y_hex_colors4["Red"] = "#9D1722"
 
 y_hex_colors1["Orange"] = "#F46A25"
-y_hex_colors2["Orange"] = "#D05A1F"
+y_hex_colors2["Orange"] = "#F46A25"
 y_hex_colors3["Orange"] = "#F68851"
 y_hex_colors4["Orange"] = "#AB4A1A"
 
 y_hex_colors1["Aqua"] = "#00A2C2"
-y_hex_colors2["Aqua"] = "#008AA5"
+y_hex_colors2["Aqua"] = "#0092AF"
 y_hex_colors3["Aqua"] = "#33B5CE"
 y_hex_colors4["Aqua"] = "#007288"
 
 y_hex_colors1["Blue"] = "#1E77D2"
-y_hex_colors2["Blue"] = "#1A65B3"
+y_hex_colors2["Blue"] = "#1B6BBD"
 y_hex_colors3["Blue"] = "#4B92DB"
 y_hex_colors4["Blue"] = "#155493"
 
@@ -69,12 +64,12 @@ y_hex_colors3["PurpleDeep"] = "#8F4AD3"
 y_hex_colors4["PurpleDeep"] = "#51148C"
 
 y_hex_colors1["Purple"] = "#AB2AD5"
-y_hex_colors2["Purple"] = "#9224B5"
+y_hex_colors2["Purple"] = "#9A26C0"
 y_hex_colors3["Purple"] = "#BC55DD"
 y_hex_colors4["Purple"] = "#781D96"
 
 y_hex_colors1["Pink"] = "#E23770"
-y_hex_colors2["Pink"] = "#C02F5F"
+y_hex_colors2["Pink"] = "#E23770"
 y_hex_colors3["Pink"] = "#E85F8D"
 y_hex_colors4["Pink"] = "#9F274F"
 
